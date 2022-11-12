767e26e2519eed65db2c0d81a3af17e7e79c799a
Daria <dariabatiuk5@gmail.com>
2022-11-12 19:05:10 -0400

    Updated JS code

--- Develop/script.js
+++ Develop/script.js
@@ -1,27 +1,36 @@
 // Assignment code here
-function generateBtn (){
+function generatePassword (){
     var passwordLength = prompt ("Choose a length of your password. It should be minimum 8 and maximum 128 characters.");
     if (passwordLength < 8 || passwordLength > 128) {
         alert ("Error");
         return;
     }
+
+    //Variables for variants of password
     var lowerCase = "abcdefghijklmnopqrstuvwxyz";
     var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numbers = "1234567890";
-    var symbol = "!@#$%^&*()?";
-    var finalPassword = '';
+    var symbols = "!@#$%^&*()?";
+    
+    //Questions to user
     var isUpperCase = confirm ("Do you want any uppercase letters?");
     var isLowerCase = confirm ("Do you want any lowercase letters?");
     var isNumbers = confirm ("Do you want any numbers?");
-    var isSymbol = confirm ("Do you want any ");
-    if (isUpperCase){
-     finalPassword += upperCase;   
-    }
-    if (isLowerCase){
-        finalPassword += upperCase;
-    }
+    var isSymbol = confirm ("Do you want any symbols?");
+
+    // Generating a string of symbols
+    var finalPassword = '';
+    var allsymbols = lowerCase + upperCase + numbers + symbols;
+
+    for (var i = 0; i < Number(passwordLength); i++){
+        var randomNumber = Math.floor(Math.random() * allsymbols.length);
+        finalPassword += allsymbols[randomNumber]; 
+     }
+
     return finalPassword;
 }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -29,10 +38,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");  
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
