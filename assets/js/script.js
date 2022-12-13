80c3ac34d56153c85b18b1226ca2ac64122b1b30
Daria <dariabatiuk5@gmail.com>
2022-12-13 19:40:59 -0400

    added if condition for selecting no character types

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,53 @@
 // Assignment code here
-function generatePassword (){
-    var passwordLength = prompt ("Choose a length of your password. It should be minimum 8 and maximum 128 characters.");
-    if (passwordLength < 8 || passwordLength > 128) {
-        alert ("Error");
-        return;
-    }
+function generatePassword() {
+  var passwordLength = prompt(
+    "Choose a length of your password. It should be minimum 8 and maximum 128 characters."
+  );
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Error");
+    return;
+  }
 
-    //Variables for variants of password
-    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numbers = "1234567890";
-    var symbols = "!@#$%^&*()?";
-    
-    //Questions to user
-    var isUpperCase = confirm ("Do you want any uppercase letters?");
-    var isLowerCase = confirm ("Do you want any lowercase letters?");
-    var isNumbers = confirm ("Do you want any numbers?");
-    var isSymbol = confirm ("Do you want any symbols?");
-    console.log();
+  //Variables for variants of password
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var symbols = "!@#$%^&*()?";
 
-    // Generating a string of symbols
-    var finalPassword = '';
-    var allsymbols = '';
+  //Questions to user
+  var isUpperCase = confirm("Do you want any uppercase letters?");
+  var isLowerCase = confirm("Do you want any lowercase letters?");
+  var isNumbers = confirm("Do you want any numbers?");
+  var isSymbol = confirm("Do you want any symbols?");
+  console.log();
 
-    if (isUpperCase){
-        allsymbols += upperCase;
-    }
-    if (isLowerCase){
-        allsymbols += lowerCase;
-    }
-    if (isNumbers){
-        allsymbols += numbers;
-    }
-    if (isSymbol){
-        allsymbols += symbols;
-    }
+  // Generating a string of symbols
+  var finalPassword = "";
+  var allsymbols = "";
 
-    for (var i = 0; i < Number(passwordLength); i++){
-        var randomNumber = Math.floor(Math.random() * allsymbols.length);
-        finalPassword += allsymbols[randomNumber]; 
-     }
+  if (isUpperCase) {
+    allsymbols += upperCase;
+  }
+  if (isLowerCase) {
+    allsymbols += lowerCase;
+  }
+  if (isNumbers) {
+    allsymbols += numbers;
+  }
+  if (isSymbol) {
+    allsymbols += symbols;
+  }
 
-    return finalPassword;
+  for (var i = 0; i < Number(passwordLength); i++) {
+    var randomNumber = Math.floor(Math.random() * allsymbols.length);
+    finalPassword += allsymbols[randomNumber];
+  }
+  //checking of there is no any entry
+  if (!isUpperCase && !isLowerCase && !isNumbers && !isSymbol) {
+    finalPassword = "Make your choise again";
+  }
+  return finalPassword;
 }
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -52,7 +55,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");  
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
